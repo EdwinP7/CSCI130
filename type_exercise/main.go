128bec5a31915086187fd677488d99dcc3441d8c
Edwin P <Edwin P>
2016-01-26 14:15:33 -0800

    Types exercise

---
+++ type_exercise/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	wheels := 4
+	rating := 5.6
+	name := "Bill"
+	cool_or_not := true
+
+	fmt.Printf("%T\n", wheels)
+	fmt.Printf("%T\n", rating)
+	fmt.Printf("%T\n", name)
+	fmt.Printf("%T\n", cool_or_not)
+
+}
