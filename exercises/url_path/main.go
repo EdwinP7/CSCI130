1c0c2428a7015bd2f27b8ea4034b9bb45e3f7ea9
Edwin P <Edwin P>
2016-03-09 08:36:55 -0800

    url req

--- exercises/url_path/main.go
+++ exercises/url_path/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"io"
 	"net/http"
+	"strings"
 )
 
 type x int
 
 func (h x) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	s1 := strings.Split(req.URL.Path, "/")
+	s2 := strings.Join(s1, " - ")
 	res.Header().Set("Content-Types", "text/html; charset=utf-8")
-	io.WriteString(res, `<h1>`+req.URL.Path+`</h1><br>`)
+	io.WriteString(res, `<h1>`+s2+`</h1><br>`)
 }
 
 func main() {
