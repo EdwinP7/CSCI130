9c7e12c8118ff50e0be70f2b2e536c87a05f5f9e
Edwin P <Edwin P>
2016-03-09 08:49:54 -0800

    name url

--- exercises/url_path/main.go
+++ exercises/url_path/main.go
@@ -1,26 +1,17 @@
 package main 
 
 import (
-	"io"
+	"fmt"
 	"net/http"
-	"strings"
 )
 
-type x int
-
-func (h x) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	s1 := strings.Split(req.URL.Path, "/")
-	s2 := strings.Join(s1, " - ")
-	res.Header().Set("Content-Types", "text/html; charset=utf-8")
-	io.WriteString(res, `<h1>`+s2+`</h1><br>`)
-}
 
 func main() {
 
-	var h x
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request){
+		res.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintf(res, "name: %v", req.URL.Path)
+	})
 
-	mux := http.NewServeMux()
-	mux.Handle("/", h)
-
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", nil)
 }
