a6b82877e1d3ee1b535620aa9e2332c8595a9e71
Edwin P <Edwin P>
2016-03-13 21:49:07 -0700

    ran fmt

--- project1/part3/main.go
+++ project1/part3/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"github.com/nu7hatch/gouuid"
 	"html/template"
 	"log"
 	"net/http"
-	"github.com/nu7hatch/gouuid"
 	//"fmt"
 )
 
@@ -20,20 +20,18 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	
 
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		
+
 		pName := req.FormValue("name")
 		pAge := req.FormValue("age")
-		
 
 		cookie, err := req.Cookie("session-fino")
-		if err != nil{
-			
+		if err != nil {
+
 			id, _ := uuid.NewV4()
 			cookie = &http.Cookie{
-				Name: "session-fino",
+				Name:  "session-fino",
 				Value: id.String() + pName + pAge,
 				//Secure: true
 				HttpOnly: true,
@@ -41,11 +39,11 @@ func main() {
 
 			http.SetCookie(res, cookie)
 		}
-			err = tpl.Execute(res, nil)
-			if err != nil {
-				http.Error(res, err.Error(), 500)
-				log.Fatalln(err)
-			}
+		err = tpl.Execute(res, nil)
+		if err != nil {
+			http.Error(res, err.Error(), 500)
+			log.Fatalln(err)
+		}
 	})
 
 	http.ListenAndServe(":8080", nil)
